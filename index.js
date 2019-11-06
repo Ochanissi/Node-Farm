6b9378f887456570e6ed3be022efdb8335e803dc
Mirel Home <mirelalex2094@gmail.com>
2019-11-06 21:54:40 +0200

    building the templates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 
 /////////////////////////////////////////////////////////
@@ -31,7 +32,30 @@ const http = require('http');
 
 /////////////////////////////////////////////////////////
 // SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObject = JSON.parse(data)
+
+
 const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is OVERVIEW.');
+    } else if (pathName === '/product') {
+        res.end('This is PRODUCT.');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
     res.end('Hello from the server!');
 });
 
